64ef1f0be7fb4e60bf1dbdecac2925e9c6b9a618
mpskeeter <mpskeeter@gmail.com>
2013-05-04 19:50:52 -0400

    Update composer to properly install.

--- NbbcBundle.php
+++ NbbcBundle.php
@@ -2,20 +2,8 @@
 
 namespace MPeters\NbbcBundle;
 
-use Symfony\Component\DependencyInjection\Compiler\PassConfig;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class NbbcBundle extends Bundle
 {
-//    /**
-//     * @see Bundle::build()
-//     *
-//     * @param ContainerBuilder $container
-//     */
-//    public function build(ContainerBuilder $container)
-//    {
-//         $container->addCompilerPass(new RegisterFiltersPass(), PassConfig::TYPE_AFTER_REMOVING);
-//         $container->addCompilerPass(new RegisterHooksPass(), PassConfig::TYPE_AFTER_REMOVING);
-//    }
 }
